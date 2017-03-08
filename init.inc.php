87eac24ef0ca79e3e1a952ba7286fb8f6fe57575
Amirreza Rahbaran <amirreza.rahbaran@rdsysco.com>
2017-03-08 13:42:47 +0330

    cleaning all debugging

--- init.inc.php
+++ init.inc.php
@@ -4,10 +4,7 @@
  * @param $class
  */
 function __autoload($class) {
-    echo "\n";
-    echo $class."\n";
     $class = "classes\\" . str_replace("\\", "/", $class) . ".class.php";
-    echo $class."\n";
     if (isset($class) && file_exists($class)) {
         /** @noinspection PhpIncludeInspection */
         require_once($class);
