502f7b18643338d1f7969ed3570795c258b8463b
Amirreza Rahbaran <amirreza.rahbaran@rdsysco.com>
2017-03-08 14:20:50 +0330

    After withdrawing money when the balance would be negative       the BankAccount get %5 negative amounts as its income

--- tests/BankAccountTest.php
+++ tests/BankAccountTest.php
@@ -30,6 +30,8 @@ class BankAccountTest extends TestCase
     public function testWithdrawWithPenalty() {
         $bank_account = new BankAccount(10);
         $bank_account->withdraw(30);
+        // percentage = (10 - 30)*(5 / 100) = 1
+        // penalty = (10 - 30) + 1 = 21
         $this->assertEquals($bank_account->getBalance(), -21);
     }
 }
